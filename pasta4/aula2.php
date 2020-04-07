914a729fc4de5512dd6795547e867ac4a1a71ed9
Luis Alberto <luis.albertosax@gmail.com>
2020-04-07 13:43:27 -0300

    commit da pasta4

---
+++ pasta4/aula2.php
@@ -0,0 +1,38 @@
+<?php 
+
+if(isset($_POST['nome']))
+{
+    $nome = $_POST['nome'];
+    file_put_contents("teste.txt", $nome, FILE_APPEND);
+
+    header("Location: aula2.php");
+}
+
+?>
+
+<form method="POST">
+    <input type="text" name="nome" />
+    <input type="submit" value="Enviar" />
+</form>    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
