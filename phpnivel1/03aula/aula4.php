ffce338c906e7952f6fb2b9563e18f22dd3cd701
Luis Alberto <luis.albertosax@gmail.com>
2020-04-09 15:09:58 -0300

    Renomeando e copiando arquivos, modificando de pastas.

---
+++ phpnivel1/03aula/aula4.php
@@ -0,0 +1,35 @@
+<?php 
+
+rename('lista.txt', 'D:\xampp2\htdocs\php\PHP0P\phpnivel1\02aula/lista2.txt');
+
+echo "Renomeou e movel o arquivo com sucesso!";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
