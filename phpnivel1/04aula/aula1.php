3222b8f4ace1ffce74a2ac6c8f4e436e46008e20
Luis Alberto <luis.albertosax@gmail.com>
2020-04-10 14:58:59 -0300

    estudando PDO

--- phpnivel1/04aula/aula1.php
+++ phpnivel1/04aula/aula1.php
@@ -4,7 +4,9 @@ $pdo = new PDO("mysql:dbname=test;hot=localhost","root", "");
 
 $sql = $pdo->query('SELECT * FROM usuarios');
 
-$dados = $sql->fetchAll();
+echo "Total: ".$sql->rowCount();
+
+$dados = $sql->fetchAll( PDO::FETCH_ASSOC );
 
 echo "<pre>";
 print_r($dados);
